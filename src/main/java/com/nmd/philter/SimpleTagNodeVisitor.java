2c456e5291e26226f54da112205ee5df3d4a03cf
Igor <nmds48@gmail.com>
2011-09-13 23:26:58 +0300

    Modified   src/main/java/com/nmd/philter/SimpleTagNodeVisitor.java

--- src/main/java/com/nmd/philter/SimpleTagNodeVisitor.java
+++ src/main/java/com/nmd/philter/SimpleTagNodeVisitor.java
@@ -2,6 +2,7 @@ package com.nmd.philter;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.htmlcleaner.ContentNode;
 import org.htmlcleaner.HtmlNode;
 import org.htmlcleaner.TagNode;
 import org.htmlcleaner.TagNodeVisitor;
@@ -19,8 +20,33 @@ public class SimpleTagNodeVisitor implements TagNodeVisitor {
     }
 
     public boolean visit(TagNode tagNode, HtmlNode htmlNode) {
-        log.info(htmlNode);
-        
+
+        if (candidateNode(tagNode)) {
+
+            if (htmlNode instanceof ContentNode) {
+                final ContentNode contentNode = (ContentNode) htmlNode;
+
+                if (contentFound(contentNode.getContent().toString())) {
+                    log.info(tagNode.getName() + " :: " + String.valueOf(contentNode.getContent().length()) +  " :: " + contentNode.getContent());
+                }
+            }
+        }
+
         return true;
+    }
+
+    private boolean candidateNode(TagNode node) {
+
+        if (node == null) {
+            return false;
+        }
+
+        return node.getName().equalsIgnoreCase("p") || node.getName().equalsIgnoreCase("div");
+    }
+
+    private boolean contentFound(String text) {
+        final String cleaned = text.replaceAll("\\s", "");
+
+        return !cleaned.isEmpty();
     }
 }
